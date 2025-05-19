d4c99cb66baacaec3a8cb275daa14e350402116c
Chase Florell <chase.florell@gmail.com>
2025-05-19 13:38:19 -0700

    [CF] checkpoint

--- build/Build.cs
+++ build/Build.cs
@@ -8,13 +8,6 @@ using Nuke.Common.ProjectModel;
 namespace AutoCollection.Build;
 [GitHubActions("continuous",
                GitHubActionsImage.MacOsLatest,
-               On = [GitHubActionsTrigger.PullRequest,],
-               InvokedTargets = [nameof(ICanTest.Test),],
-               CacheKeyFiles = ["**/global.json", "**/Directory.Packages.props",],
-               ImportSecrets = [nameof(NugetApiKey)],
-               EnableGitHubToken = true)]
-[GitHubActions("main",
-               GitHubActionsImage.MacOsLatest,
                On = [GitHubActionsTrigger.Push,],
                InvokedTargets = [nameof(ICanPublish.Publish),],
                CacheKeyFiles = ["**/global.json", "**/Directory.Packages.props",],
@@ -38,6 +31,4 @@ class Build
 	public static int Main() => Execute<Build>(x => x.Run);
 
 	Target Run => target => target.DependsOn<ICanTest>(static x => x.Test);
-
-	private static void TriggerAssemblyResolution() => _ = new ProjectCollection();
 }
