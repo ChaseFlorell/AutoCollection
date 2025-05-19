ac103c8ada38a25e4c226de1fa5046ce05518ad4
Chase Florell <chase.florell@gmail.com>
2025-05-19 16:11:16 -0700

    [CF] improves readme and adds docs to generated attribute

--- tests/AutoCollection.UnitTests/Models/PersonList.cs
+++ tests/AutoCollection.UnitTests/Models/PersonList.cs
@@ -2,3 +2,20 @@ namespace AutoCollection.UnitTests.Models;
 /// <summary>Generate a Person ReadOnlyList implementation. <see cref="Person" /> is a complex type.</summary>
 [GenerateReadOnlyList(typeof(Person))]
 public partial class PersonList;
+
+#region PersonObject
+
+public record Person(string FirstName, string LastName)
+{
+	public string FullName => $"{FirstName} {LastName}";
+}
+
+public sealed class PersonFixture
+{
+	private readonly string _firstName = "John";
+	private readonly string _lastName = "Doe";
+	public static implicit operator Person(PersonFixture fixture) => fixture.Build();
+	private Person Build() => new(_firstName, _lastName);
+}
+
+#endregion
