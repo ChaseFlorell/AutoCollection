cdb76289ed6e3d2bf28462554e63984b58ca569d
Chase Florell <chase.florell@gmail.com>
2025-05-20 19:33:09 -0700

    [S][#6] Fixes deployment path issue

--- build/Targets/ICanPublish.cs
+++ build/Targets/ICanPublish.cs
@@ -4,17 +4,18 @@ using Nuke.Common.Git;
 using Nuke.Common.Tools.DotNet;
 
 namespace AutoCollection.Build.Targets;
+
 public interface ICanPublish : IHaveConfiguration
 {
 	Target Publish =>
 		d => d
-		     .Description("Publish Nuget")
-		     .DependsOn<ICanTest>(x => x.Test)
-		     .OnlyWhenDynamic(() => Repository.IsOnMainOrMasterBranch() && GitHubActions.Instance is {})
-		     .WhenSkipped(DependencyBehavior.Skip)
-		     .Executes(() => DotNetTasks.DotNetNuGetPush(cfg => cfg
-		                                                        .SetApiKey(NugetApiKey)
-		                                                        .SetSource("https://api.nuget.org/v3/index.json")
-		                                                        .SetTargetPath(BuildArtifactsDirectory / $"AutoCollection.{Version}.nupkg")
-		                                                        .SetSymbolSource(BuildArtifactsDirectory / $"AutoCollection.{Version}.snupkg")));
+			.Description("Publish Nuget")
+			.DependsOn<ICanTest>(x => x.Test)
+			.OnlyWhenDynamic(() => Repository.IsOnMainOrMasterBranch() && GitHubActions.Instance is { })
+			.WhenSkipped(DependencyBehavior.Skip)
+			.Executes(() => DotNetTasks.DotNetNuGetPush(cfg => cfg
+				.SetApiKey(NugetApiKey)
+				.SetSource("https://api.nuget.org/v3/index.json")
+				.SetTargetPath(BuildArtifactsDirectory / "AutoCollection" / $"AutoCollection.{Version}.nupkg")
+				.SetSymbolSource(BuildArtifactsDirectory / "AutoCollection" / $"AutoCollection.{Version}.snupkg")));
 }
