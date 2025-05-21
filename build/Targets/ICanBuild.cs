e66125314f39abd990d0bad7a28d2e6d6dafca98
Chase Florell <chase.florell@gmail.com>
2025-05-20 19:26:02 -0700

    [S][#6] Adds proper editorconfig values and also fixes all build warnings

--- build/Targets/ICanBuild.cs
+++ build/Targets/ICanBuild.cs
@@ -8,17 +8,27 @@ public interface ICanCompile : IHaveConfiguration
 {
 	Target Compile =>
 		target => target
-		          .Description("Build Solution")
-		          .DependsOn<ICanInitialize>(static x => x.Initialize)
-		          .Executes(() => DotNetTasks
-			                    .DotNetBuild(s => s
-			                                      .SetProjectFile(Solution)
-			                                      .SetAssemblyVersion(Version)
-			                                      .SetFileVersion(__todaysBuild)
-			                                      .SetVersion(Version)
-			                                      .SetInformationalVersion(Version)
-			                                      .SetOutputDirectory(BuildArtifactsDirectory)
-			                                      .SetConfiguration(Configuration)));
+			.Description("Build Projects")
+			.DependsOn<ICanInitialize>(static x => x.Initialize)
+			.Executes(() =>
+			{
+				// Get all projects in the solution
+				var projects = Solution!.AllProjects;
+
+				// Build each project separately
+				foreach (var project in projects)
+				{
+					DotNetTasks.DotNetBuild(s => s
+						.SetProjectFile(project)
+						.SetAssemblyVersion(Version)
+						.SetFileVersion(__todaysBuild)
+						.SetVersion(Version)
+						.SetInformationalVersion(Version)
+						.SetOutputDirectory(BuildArtifactsDirectory / project.Name)
+						.SetConfiguration(Configuration));
+				}
+			});
+
 
 	private static readonly string __todaysBuild = $"{DateTime.Today:yyyy.MM.dd}.{GitHubActions.Instance?.RunNumber ?? 0}";
 }
